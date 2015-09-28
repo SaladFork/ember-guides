4b6697cf507386001c296e4884133d278cc74fe1
Elad Shahar <SaladFork@Gmail.com>
2015-09-27 21:04:48 -0400

    Add warning about viewing an old version of the guides
    A cherry pick of 7144d62 with modifications for different
    templating.
    
    Since this version of the guides will always be outdated, we do not
    include the logic for detecting if viewing an old version.

--- source/javascripts/common-all.js
+++ source/javascripts/common-all.js
@@ -1,6 +1,9 @@
 //= require vendor/getDevicePixelRatio-min
 //= require vendor/select2
 //= require app/back-to-top.js
+//= require app/_versions.js.erb
+//= require app/version-select.js
+//= require app/outdated-warnings.js
 //= require underscore
 //= require vendor/jquery.swiftype.autocomplete
 //= require swiftype.js
